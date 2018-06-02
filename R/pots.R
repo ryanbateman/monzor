aad050278623b0997dc9bc714bdbb69f084353b8
ryanbateman <ryan.bateman@gmail.com>
2018-06-02 18:14:47 +0200

    Added ability to deposit into a pot

--- R/pots.R
+++ R/pots.R
@@ -14,3 +14,21 @@ getPots <- function(mtoken = getMonzoToken(), accountId = NULL) {
     potsJson = fromJSON(content(potsRequest, type = "text"))
     potsJson
 }
+
+#' Deposit in a pot
+#' @param mtoken The Monzo API token
+#' @param accountId The source id of the account you're transferring from
+#' @param amount The amount to deposit in the pot
+#' @param dedupeId The id required to ensure the deposit is unique
+#' @keywords pots
+#' @import httr jsonlite
+#' @export
+depositIntoPot <- function(mtoken = getMonzoToken(), accountId = NULL, potId = NULL, amount = NULL, dedupeId = NULL) {
+    if (is.null(accountId)) {
+        accountId <- getDefaultAccountId(mtoken)
+    }
+    url = paste("https://api.monzo.com/pots/", potId, "/deposit", sep="")
+    depositIntoPotRequest <- PUT(url, config(token = mtoken), body = list("source_account_id" = accountId, "amount" = amount, "dedupe_id" = dedupeId), verbose(), encode = "form")
+    depositIntoPotJson = fromJSON(content(depositIntoPotRequest, type = "text"))
+    depositIntoPotJson
+}
