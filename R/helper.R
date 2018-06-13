4c3ea923cdff1419eac491883bbec88b5e74b6f4
ryanbateman <ryan.bateman@gmail.com>
2018-06-13 12:14:49 +0200

    Removed deprecated helper class

--- R/helper.R
+++
@@ -1,8 +0,0 @@
-testToken <- "<Token>
-<oauth_endpoint>
-authorize: https://auth.monzo.com/
-access:    https://api.monzo.com/oauth2/token
-<oauth_app> monzor
-key:    nothing
-secret: nothing
-<credentials> access_token, client_id, expires_in, refresh_token, scope, token_type, user_id"
